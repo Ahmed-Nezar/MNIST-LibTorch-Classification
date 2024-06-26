abdd35c02f32baecfe8436e5589431b8625e253d
Ahmed-Nezar <ahmednezarrr@gmail.com>
2024-06-26 05:20:59 +0300

    feat: adding main function

--- main.cpp
+++ main.cpp
@@ -1,34 +1,38 @@
-#include <torch/torch.h>
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include "utills.hpp"
 
-using namespace cv; 
 using namespace std;
 
 int main() {
-    torch::Tensor tensor = torch::rand({2, 3});
-    cout << tensor << endl;
-    
-    Mat image = imread("../../../../image.png",  // Path can be changed according to the location of the image
-                       IMREAD_COLOR);
-    // Error Handling 
-    if (image.empty()) { 
-        cout << "Image File "
-             << "Not Found" << endl; 
-  
-        // wait for any key press 
-        cin.get(); 
-        return -1; 
-    } 
-  
-    // Show Image inside a window with 
-    // the name provided 
-    imshow("Window Name", image); 
-    
-    cout << "Image Size" << image.size() << endl;
-    // Wait for any keystroke 
-    waitKey(0); 
-    return 0; 
 
+    // Define Cuda device
+    torch::Device device(torch::kCUDA);
+    torch::manual_seed(42);
 
+    NeuralNetwork model(784, 10);
+    model.to(device);
+    
+    // Defining Adam Optimizer
+    torch::optim::Adam optimizer(model.parameters(), torch::optim::AdamOptions(1e-3));
+
+    // Defining Cross Entropy Loss
+    torch::nn::CrossEntropyLoss criterion;
+
+    // Loading MNIST dataset training data
+    auto dataset = torch::data::datasets::MNIST("./MNIST")
+        .map(torch::data::transforms::Stack<>());
+
+    auto train_data_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+        std::move(dataset), 64);
+
+    auto valid_dataset = torch::data::datasets::MNIST("./MNIST", torch::data::datasets::MNIST::Mode::kTest)
+        .map(torch::data::transforms::Stack<>());
+    auto valid_dataloader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+        std::move(valid_dataset), 64);
+
+    int epochs = 10;
+    train_model(model, epochs, train_data_loader,valid_dataloader,optimizer, criterion, device);
+    
+    save_model(model, "model.pth");
+
+    return 0;
 }
